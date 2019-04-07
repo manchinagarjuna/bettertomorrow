11051f64d4dc3f03a7359091c7c9d1b89eb4b7c5
Nagarjuna Manchineni <manchinagarjuna@Nagarjunas-MacBook-Pro.local>
2019-04-07 09:28:10 -0400

    Adding MongoDB connectivity Adding API handlers for users and posts Removing the mongodb vendoring

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
-	"net/http"
-	"fmt"
+	"context"
+	"encoding/json"
 	"log"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/manchinagarjuna/bettertomorrow/pkg/user"
+	"github.com/manchinagarjuna/bettertomorrow/pkg/util"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // Basic logger to help with debugging missing static page errors.
@@ -14,23 +22,68 @@ func logger(h http.Handler) http.Handler {
 	})
 }
 
+var userCollection *mongo.Collection
+var orgCollection *mongo.Collection
+var eventCollection *mongo.Collection
+
+func apiHandler(w http.ResponseWriter, r *http.Request) {
+
+	decoder := json.NewDecoder(r.Body)
+	var requestMessage util.RequestMessage
+	err := decoder.Decode(&requestMessage)
+	if err != nil {
+		response := util.ResponseMessage{Data: "", Error: err.Error()}
+		res, _ := json.Marshal(response)
+		w.Write([]byte(res))
+		return
+	}
+
+	switch strings.TrimRight(strings.TrimPrefix(r.URL.Path, "/api/v1/"), "/") {
+	case "users":
+		go user.UserHandler(w, requestMessage, userCollection, r.Method)
+	}
+}
+
+func connectToMongoDB() (*mongo.Database, error) {
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb+srv://mongouser:bettertomorrow@bettertomorrow-ffmrf.gcp.mongodb.net/test?retryWrites=true"))
+
+	if err != nil {
+		return nil, err
+	}
+
+	database := client.Database("bettertomorrow")
+
+	return database, nil
+}
+
 func main() {
 
+	port := "8080"
+
+	database, err := connectToMongoDB()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	userCollection = database.Collection("user")
+	orgCollection = database.Collection("organization")
+	eventCollection = database.Collection("event")
+
 	// Creating a ServeMux to log messages as well as serve static pages.
-	h := http.NewServeMux()
+	server := http.NewServeMux()
 
-	// Serving static assets.
+	//Serving static assets
 	fs := http.FileServer(http.Dir("assets/static"))
-	h.Handle("/", fs)
-
 	// Connect a basic logger.
-	handlerWithLogger := logger(h)
+	handlerWithLogger := logger(fs)
+	server.Handle("/", fs)
+	http.HandleFunc("/api/v1/", apiHandler)
 
-	// Serve at port 8080 on localhost.
-	var port = "8080"
-	fmt.Println("Serving on port " + port)	
-	err := http.ListenAndServe(":" + port, handlerWithLogger)
+	log.Println("Serving on port " + port)
+	err = http.ListenAndServe(":"+port, handlerWithLogger)
 
-	// On failure, print the error message instead of existing quietly.
-	log.Fatal(err)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 }
---
+++ pkg/user/handler.go
@@ -0,0 +1,92 @@
+package user
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/manchinagarjuna/bettertomorrow/pkg/util"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func UserHandler(w http.ResponseWriter, msg util.RequestMessage, collection *mongo.Collection, httpMethod string) {
+	var data interface{}
+	var err error
+	errorString := ""
+
+	switch msg.API {
+	case "get":
+		data, err = GetUsers(collection)
+	case "insert":
+		var user User
+		err = json.Unmarshal(msg.Data, &user)
+		if err != nil {
+			break
+		}
+		err = InsertUser(collection, user)
+	case "delete":
+		var user User
+		err = json.Unmarshal(msg.Data, &user)
+		if err != nil {
+			break
+		}
+		err = DeleteUser(collection, user)
+	}
+
+	if err != nil {
+		errorString = err.Error()
+	}
+
+	response := util.ResponseMessage{Data: data, Error: errorString}
+	res, _ := json.Marshal(response)
+	w.Write([]byte(res))
+}
+
+func GetUsers(collection *mongo.Collection) ([]User, error) {
+	var users []User
+	cur, err := collection.Find(context.Background(), bson.D{})
+	if err != nil {
+		return users, err
+	}
+
+	defer cur.Close(context.Background())
+	for cur.Next(context.Background()) {
+		var user *User = &User{}
+		err := cur.Decode(user)
+		if err != nil {
+			return users, err
+		}
+		users = append(users, *user)
+	}
+	err = cur.Err()
+	return users, err
+}
+
+func InsertUser(collection *mongo.Collection, user User) error {
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	userBson, err := bson.Marshal(user)
+	if err != nil {
+		return err
+	}
+
+	res, err := collection.InsertOne(ctx, userBson)
+	id := res.InsertedID
+	fmt.Println(id)
+
+	return err
+}
+
+func DeleteUser(collection *mongo.Collection, user User) error {
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	userBson, err := bson.Marshal(user)
+	if err != nil {
+		return err
+	}
+
+	_, err = collection.DeleteOne(ctx, userBson)
+
+	return err
+}
